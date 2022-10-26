0b5583b5cd27f7d61265cb7df03f94e22acf5190
sanjay <sanjaykumar73189@gmail.com>
2022-10-26 23:52:42 +0530

    using pusher

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 import express from "express";
 import mongoose from "mongoose";
 import Messages from "./dbmessages.js";
+import Pusher from "pusher";
+
 const app = express();
 const port = process.env.PORT || 9000;
+
+const pusher = new Pusher({
+    appId: "1497708",
+    key: "9e3a199104fea69bb83a",
+    secret: "31390be9aecf8ef18862",
+    cluster: "ap2",
+    useTLS: true
+});
 
 const connection_url="mongodb+srv://admin:huvkam5RhzeoU1eH@cluster1.4ruxgjo.mongodb.net/?retryWrites=true&w=majority"
 
@@ -15,6 +25,26 @@ mongoose.connect(connection_url,{
     useUnifiedTopology:true
 })
 
+const db=mongoose.connection
+
+db.once("open",()=>{
+    console.log("DB Connected");
+    const msgCOllection= db.collection("messagecontents");
+    const changeStream = msgCOllection.watch();
+
+    changeStream.on("change",(change)=>{
+        console.log(change)
+        if(change.operationType==="insert"){
+            const messageDetails=change.fullDocument;
+            pusher.trigger("messages","inserted",{
+                name:messageDetails.user,
+                message:messageDetails.message
+            });
+        }else{
+            console.log("error Triggering pusher");
+        }
+    })
+})
 
 app.get("/", (req, res) => {
     res.status(200).send("Hello")
